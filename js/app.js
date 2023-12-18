939f218ffff819f2a67ff6eb2b2ffe335c06eef7
Bruno cordoba <brunonahuel87@gmail.com>
2023-12-18 09:37:24 -0300

    agregue logica para local storage no quede vacio

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let nombre = document.getElementById("nombre");
 let categoria = document.getElementById("categoria");
 let descripcion = document.getElementById("descripcion");
 let publicado = document.getElementById("subidoPagina");
-const peliculas = [];
+const peliculas = JSON.parse(localStorage.getItem('pelisKey'))|| [];
 
 function limpiarFormulario() {
   formulario.reset();
@@ -16,51 +16,16 @@ function limpiarFormulario() {
 function guardarLocalStore() {
   localStorage.setItem("pelisKey", JSON.stringify(peliculas));
 }
-
-function crearContacto(e) {
-  e.preventDefault();
-  const pelicula = new Pelicula(nombre.value,categoria.value,publicado.value,descripcion.value);
-  peliculas.push(pelicula);
-  guardarLocalStore();
-  
-
-  
-  //agregar texto a tabla
-  const nuevaCelda = document.createElement('tr');
-  if (publicado.value == 'Si' || publicado.value == 'si') {
-    nuevaCelda.innerHTML = ` <th scope="row">${crypto.randomUUID()}</th>
-  <td>${nombre.value}</td>
-  <td>${categoria.value}</td>
+function dibujarCelda() {
+  const contenedorPadre = document.querySelector('tbody');
+  contenedorPadre.innerHTML += ` <tr>
+  <th scope="row">313123</th>
+  <td>Napoleón</td>
+  <td>Drama Historia Guerra</td>
   <td>
     <p>
-    ${descripcion.value}
-    </p>
-  </td>
-  <td>
-  <i class="bi bi-file-earmark-check-fill fs-4 text-success"></i>
-  </td>
-  <td >
-    <button type="button" title="Editar" class="btn lapiz fs-4 text-info">
-      <i class="bi bi-pencil-square"></i>
-    </button>
-    <button
-      type="submit"
-      title="Destacar"
-      class="btn estrella fs-4 text-warning"
-    >
-      <i class="bi bi-star-fill"></i>
-    </button>
-    <button type="button" title="Eliminar" class="btn tacho fs-4 text-secondary">
-      <i class="bi bi-trash-fill"></i>
-    </button>
-  </td>`; 
-  }else{
-    nuevaCelda.innerHTML = ` <th scope="row">${crypto.randomUUID()}</th>
-  <td>${nombre.value}</td>
-  <td>${categoria.value}</td>
-  <td>
-    <p>
-    ${descripcion.value}
+      Una mirada personal a los orígenes del líder militar francés
+      y su rápido y despiadado ascenso a emperador.
     </p>
   </td>
   <td>
@@ -83,12 +48,19 @@ function crearContacto(e) {
     <button type="button" title="Eliminar" class="btn tacho fs-4 text-secondary">
       <i class="bi bi-trash-fill"></i>
     </button>
-  </td>`; 
-  }
+  </td>
+</tr>`
+}
 
+function crearContacto(e) {
+  e.preventDefault();
+  const pelicula = new Pelicula(nombre.value,categoria.value,publicado.value,descripcion.value);
+  peliculas.push(pelicula);
+  guardarLocalStore();
+  
 
-  const contenedorPadre = document.querySelector("tbody");
-  contenedorPadre.appendChild(nuevaCelda);
+  
+  
 
   limpiarFormulario();
 }
